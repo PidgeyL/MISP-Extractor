4f06c3951cae735843db398c976925711ac28ed9
PidgeyL <pieterjan.moreels@gmail.com>
2016-06-22 14:08:49 +0200

    add more options for the key and url

--- MISP-Extractor.py
+++ MISP-Extractor.py
@@ -2,6 +2,7 @@
 # Default Imports
 import csv
 import json
+import os
 import sys
 
 from io import StringIO
@@ -42,6 +43,19 @@ threat_level = {4: "Undefined",
 #############
 # Functions #
 #############
+def _loadSettings():
+  global url
+  global key
+  # See if args passed
+  if not url: url = args.u
+  if not key: key = args.k
+  # Check environment variables
+  if not url: url = os.getenv("misp_url")
+  if not key: key = os.getenv("misp_key")
+  # Exit if necessary
+  if not url: sys.exit("Could not find the MISP API URL")
+  if not key: sys.exit("Could not find the MISP API key")
+
 def _generateCSV(output):
   # Make CSV in memory
   memoryFile = StringIO()
@@ -123,15 +137,18 @@ if __name__ == '__main__':
   argParser.add_argument('-t',                      action="store_true", help='Get DataTypes (Debug option')
   argParser.add_argument('-e',                      action="store_true", help='Get Examples (Debug option')
   argParser.add_argument('-o', metavar="file",      type=str,            help='Output file (default stdout)')
+  argParser.add_argument('-u', metavar="MISP URL",  type=str,            help='The URL to the MISP API')
+  argParser.add_argument('-k', metavar="MISP Key",  type=str,            help='Your API key for the MISP API')
   argParser.add_argument('-H',                      action="store_true", help='Include headers to the output')
   argParser.add_argument('-S', metavar="separator", type=str,            help='Separator for the output (default ",")')
   argParser.add_argument('-A', metavar="integer",   type=int,            help='Analysis level (0: Initial, 1: Ongoing, 2: Completed)')
   argParser.add_argument('-T', metavar="integer",   type=int,            help='Threat level (4: Undefined, 3: Low, 2: Medium, 1: High)')
   args = argParser.parse_args()
 
-  if not args.t or args.e or args.d:
+  if not (args.t or args.e or args.d):
     sys.exit("Please choose an option")
 
+  _loadSettings()
   data = getMISPData(url, key)
   
   if args.t:
